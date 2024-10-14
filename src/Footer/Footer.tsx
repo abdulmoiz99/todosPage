031dd3cd7e18dc4c7e68f6f1f4317449bcdabe72
Abdul Moiz <moiza0799@gmail.com>
2024-10-14 11:39:02 -0500

    Added toggle for todo

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -2,14 +2,24 @@ import { useTodoContext } from "../TodoContext";
 import "./Footer.css";
 
 export function Footer() {
-  const { Todo: todo } = useTodoContext();
+  const { Todo: todo, setTodo } = useTodoContext();
   const completedTodoCount = todo.filter((item) => item.completed).length;
+
+  const updateAllTodo = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = event.target.checked;
+
+    const updatedTodo = todo.map((item) => ({
+      ...item,
+      completed: isChecked,
+    }));
+    setTodo(updatedTodo);
+  };
 
   return (
     <>
       <div className="todo-footer">
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" onChange={updateAllTodo}/>
         </label>
         <span>
           <span>Finished {completedTodoCount}</span> / total {todo.length}
