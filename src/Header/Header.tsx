23b13b544e8e80693575819d40b2882a18826e6c
Abdul Moiz <moiza0799@gmail.com>
2024-10-14 11:50:44 -0500

    Added option to add new todo

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,36 @@
-import './Header.css'
+import { useState } from "react";
+import { useTodoContext } from "../TodoContext";
+import { Todo } from "../Model/Todo";
+import "./Header.css";
 
 export function Header() {
+  const { Todo, setTodo } = useTodoContext();
+  const [todoTask, setTodoTask] = useState("");
+
+  const addNewEvent = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const newTodo: Todo = {
+        id: Todo[Todo.length - 1].id + 1,
+        task: todoTask,
+        completed: false,
+      };
+
+      setTodo([...Todo, newTodo]);
+      setTodoTask("");
+    }
+  };
+
   return (
     <>
       <div className="todo-header">
-        <input type="text" placeholder="Enter task name" />
+        <input
+          name="todoTask"
+          type="text"
+          placeholder="Enter task name"
+          onKeyDown={addNewEvent}
+          value={todoTask}
+          onChange={(e) => setTodoTask(e.target.value)}
+        />
       </div>
     </>
   );
